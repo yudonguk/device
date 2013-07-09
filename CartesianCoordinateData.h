89b9185f062d7a2613d094e6269fcda95a2bbe97
Hong Seong PARK <hspark@kangwon.ac.kr>
2013-07-09 15:40:40 +0900

    Update CartesianCoordinateData.h

--- CartesianCoordinateData.h
+++ CartesianCoordinateData.h
@@ -8,9 +8,10 @@ namespace OPRoS
 class CartesianCoordinate
 {
 public:
-	CartesianCoordinate() {
+	CartesianCoordinate(void) {
 		x = 0.0;		y = 0.0;		z = 0.0;
 		roll = 0.0;		pitch = 0.0;	yaw = 0.0;
+		status = OPROS_SUCCESS;
 	}
 	CartesianCoordinate(float64_t x, float64_t y, float64_t z, float64_t roll, float64_t pitch, float64_t yaw) {
 		this->x = x;				this->y = y;				this->z = z;
